a99ee70050cf237bc22316e20ba628e0b21ca925
Fredrik Frodlund <fredrik@frodlund.se>
2019-07-08 12:52:35 +0200

    Add support for CSS source maps

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,15 @@
-/* eslint global-require:0 */
+/* eslint-disable */
 
 module.exports = {
   map: { inline: true },
   plugins: [
-    require('postcss-custom-properties'),
+    require('postcss-import-ext-glob'),
     require('postcss-import'),
+    require('postcss-custom-properties'),
+    require('cssnano')({
+      preset: ['default', {
+        colormin: false
+      }]
+    }),
   ]
 };
